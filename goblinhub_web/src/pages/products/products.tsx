282f601f44d9d0dba75a732808a7ae00cfbc8e1c
Adrián Santos <a23311062@uthermosillo.edu.mx>
2026-03-06 23:13:19 -0700

    fix(productos): se soluciono el error de Eslint

--- goblinhub_web/src/pages/products/products.tsx
+++ goblinhub_web/src/pages/products/products.tsx
@@ -1,7 +1,7 @@
-import { useEffect, useState } from "react";
+import { useState } from "react";
 import { Link } from "react-router-dom";
 import "./products.css";
-import  images  from "../../assets/images.jpg";
+import images from "../../assets/images.jpg";
 
 interface Producto {
   id: number;
@@ -15,54 +15,52 @@ interface Producto {
 }
 
 function ProductsPage() {
-  const [productos, setProductos] = useState<Producto[]>([]);
-
-  useEffect(() => {
-    const data: Producto[] = [
-      {
-        id: 1,
-        nombre: "Space Marine Battle Sector",
-        precio: 1200,
-        imagen: images,
-        popular: true,
-        categoria: "Wargames",
-        descripcion: "Juego de estrategia basado en el universo de Warhammer 40K.",
-      },
-      {
-        id: 2,
-        nombre: "Tyranid Prime",
-        precio: 800,
-        imagen: "https://via.placeholder.com/400x300",
-        categoria: "Wargames",
-        descripcion: "Miniatura del comandante Tyranid para Warhammer 40K.",
-      },
-      {
-        id: 3,
-        nombre: "Roboute Guilliman",
-        precio: 3500,
-        imagen: "https://via.placeholder.com/400x300",
-        nuevo: true,
-        categoria: "Wargames",
-        descripcion: "Primarca de los Ultramarines para Warhammer 40K.",
-      },
-      {
-        id: 4,
-        nombre: "Rey silente",
-        precio: 1200,
-        imagen: "https://via.placeholder.com/400x300",
-        popular: true,
-        categoria: "Wargames",
-        descripcion: "Gran señor de los necrones, dinastia Sharekan",
-      },
-    ];
-
-    setProductos(data);
-  }, []);
+  const [productos] = useState<Producto[]>([
+    {
+      id: 1,
+      nombre: "Space Marine Battle Sector",
+      precio: 1200,
+      imagen: images,
+      popular: true,
+      categoria: "Wargames",
+      descripcion: "Juego de estrategia basado en el universo de Warhammer 40K.",
+    },
+    {
+      id: 2,
+      nombre: "Tyranid Prime",
+      precio: 800,
+      imagen: "https://via.placeholder.com/400x300",
+      categoria: "Wargames",
+      descripcion: "Miniatura del comandante Tyranid para Warhammer 40K.",
+    },
+    {
+      id: 3,
+      nombre: "Roboute Guilliman",
+      precio: 3500,
+      imagen: "https://via.placeholder.com/400x300",
+      nuevo: true,
+      categoria: "Wargames",
+      descripcion: "Primarca de los Ultramarines para Warhammer 40K.",
+    },
+    {
+      id: 4,
+      nombre: "Rey Silente",
+      precio: 1200,
+      imagen: "https://via.placeholder.com/400x300",
+      popular: true,
+      categoria: "Wargames",
+      descripcion: "Gran señor de los necrones, dinastía Szarekhan.",
+    },
+  ]);
 
   return (
     <div className="product-page">
       <h1 className="products-title">Nuestro inventario</h1>
-      <h2 className="products-subtitle">Explora nuestra selección de wargames, juegos de rol, juegos de mesa y accesorios. Control de stock en tiempo real. </h2>
+      <h2 className="products-subtitle">
+        Explora nuestra selección de wargames, juegos de rol, juegos de mesa y accesorios. 
+        Control de stock en tiempo real.
+      </h2>
+
       <div className="list-category">
         <button className="category-button">Wargames</button>
         <button className="category-button">Juegos de rol</button>
@@ -70,6 +68,7 @@ function ProductsPage() {
         <button className="category-button">Pinturas</button>
         <button className="category-button">Accesorios</button>
       </div>
+
       <div className="products-grid">
         {productos.map((producto) => (
           <Link
